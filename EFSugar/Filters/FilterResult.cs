f3471db27d4eec555b1306cca6fc060691348071
muchman <mike@greyscale.io>
2018-09-21 12:43:47 -0400

    restructured files and added filters

---
+++ EFSugar/Filters/FilterResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFSugar.Filters
+{
+    public class FilterResult<T>
+    {
+        public T Result { get; set; }
+        public int RecordCount { get; set; }
+    }
+}
---
+++ EFSugar/Filters/PagingFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EFSugar.Filters
+{
+    public class PagingFilter<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public FilterResult<IQueryable<T>> ApplyFilter(IQueryable<T> query)
+        {
+            var result = new FilterResult<IQueryable<T>>();
+            result.RecordCount = query.Count();
+            result.Result = query.Skip((PageNumber > 0 ? PageNumber - 1 : 0) * PageSize).Take(PageSize);
+            return result;
+        }
+    }
+}
