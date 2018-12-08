219c29cebe8198fc2b75ae4f33b3331251dfe5c9
muchman <mike@greyscale.io>
2018-12-08 16:17:20 -0500

    removed a lot of the param rebinder stuff!  Performance!

--- EFSugar/Extensions/ExpressionExtensions.cs
+++ EFSugar/Extensions/ExpressionExtensions.cs
@@ -7,46 +7,40 @@ using System.Text;
 
 namespace EFSugar.Filters
 {
-    /* Note: You dont need this Expression stuff at all, it is just syntax sugar.  Otherwise you write stuff like:
-     *
-     * Func<Entity, bool> = o => o.Name != null ? o.Name == "something" : true && o.Age.HasValue? o.Age.Value > 21 : true
-     *
-     * and you keep building this if(thing) expression else true and honestly that is hard to debug because it is evaluated as a single unit.
-     * With this we can actually break up the expressions and only add them when it is required instead of filling in a "else true" just to keep
-     * the string of expressions going
+    /* Note: Yes I have commented code here, it will be removed soon!
+
      */
 
     public static class ExpressionExtensions
     {
         //We need this Compose as support for the AND and or OR calls because you cannot just blindly compose to unknown expressions.  More explanation on the ParameterRebinder.
-        public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        internal static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
         {
-            // build parameter map (from parameters of second to parameters of first)
-            var map = first.Parameters.Select((paramExpression, index) => new { firstParam = paramExpression, secondParam = second.Parameters[index] }).ToDictionary(p => p.secondParam, p => p.firstParam);
+           // build parameter map (from parameters of second to parameters of first)
+           // var map = first.Parameters.Select((paramExpression, index) => new { firstParam = paramExpression, secondParam = second.Parameters[index] }).ToDictionary(p => p.secondParam, p => p.firstParam);
 
             // replace parameters in the second lambda expression with parameters from the first
-            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
 
             // apply composition of lambda expression bodies to parameters from the first expression
-            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+            return Expression.Lambda<T>(merge(first.Body, second.Body), first.Parameters);
         }
 
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        internal static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
         {
             return first.Compose(second, Expression.AndAlso);
         }
 
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        internal static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
         {
             return first.Compose(second, Expression.Or);
         }
 
-        public static bool IsAssigned<T>(this T item)
+        internal static bool IsAssigned<T>(this T item)
         {
             return !(EqualityComparer<T>.Default.Equals(item, default(T)));
         }
 
-        public static bool IsAssigned<T>(this T item, PropertyInfo prop)
+        internal static bool IsAssigned<T>(this T item, PropertyInfo prop)
         {
             if (prop.PropertyType.GetConstructor(Type.EmptyTypes) == null)
             {
@@ -77,30 +71,45 @@ namespace EFSugar.Filters
      * you are passing in.
      */
 
-    internal class ParameterRebinder : ExpressionVisitor
-    {
-        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+    //internal class BasicRebinder : ExpressionVisitor
+    //{
+    //    private readonly ParameterExpression _parameter;
 
-        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
-        {
-            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
-        }
+    //    protected override Expression VisitParameter(ParameterExpression node)
+    //    {
+    //        return base.VisitParameter(_parameter);
+    //    }
 
-        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
-        {
-            return new ParameterRebinder(map).Visit(exp);
-        }
+    //    internal BasicRebinder(ParameterExpression parameter)
+    //    {
+    //        _parameter = parameter;
+    //    }
+    //}
 
-        protected override Expression VisitParameter(ParameterExpression p)
-        {
-            ParameterExpression replacement;
+    //internal class ParameterRebinder : ExpressionVisitor
+    //{
+    //    private readonly Dictionary<ParameterExpression, ParameterExpression> map;
 
-            if (map.TryGetValue(p, out replacement))
-            {
-                p = replacement;
-            }
+    //    public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+    //    {
+    //        this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+    //    }
 
-            return base.VisitParameter(p);
-        }
-    }
+    //    public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+    //    {
+    //        return new ParameterRebinder(map).Visit(exp);
+    //    }
+
+    //    protected override Expression VisitParameter(ParameterExpression p)
+    //    {
+    //        ParameterExpression replacement;
+
+    //        if (map.TryGetValue(p, out replacement))
+    //        {
+    //            p = replacement;
+    //        }
+
+    //        return base.VisitParameter(p);
+    //    }
+    //}
 }
